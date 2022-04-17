cee881636977d9a6cccc096612b55bc621284135
Argón <70592958+TheOriginalArgon@users.noreply.github.com>
2022-04-17 14:06:49 -0300

    v2.3
    -Fixed patching bugs with some VE mods.
    -VCE canning no longer requires steel.
    -Changed preview image.
    -Added new auto-patching functions.
    -Integrated oil extractor from EM - Plastics.

---
+++ Source/MaterialPatchingUtility/MaterialPatchingUtility/PatchOperationApplyHardCodedMaterialPatch.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using Verse;
+
+namespace MaterialPatchingUtility
+{
+    class PatchOperationApplyHardCodedMaterialPatch : PatchOperation
+    {
+        public ThingDef resource;
+
+        protected override bool ApplyWorker(XmlDocument xml)
+        {
+            ThingDefCountClass res = new ThingDefCountClass();
+            res.thingDef = resource;
+            List<ThingDef> patchableThings = DefDatabase<ThingDef>.AllDefsListForReading;
+
+            patchableThings.Remove(from q in patchableThings where !q.Minifiable && q.costList.Contains())
+        }
+    }
+}
